46614d1e3666eda0d82ae0e91c38b8a469397e1e
Erik Garrison <erik.garrison@gmail.com>
2021-04-14 15:41:32 +0200

    update burn script

--- burn_extract.sh
+++ burn_extract.sh
@@ -3,31 +3,43 @@
 set -eo pipefail
 
 f=$1
+ref=$2
+threads=$3
+# 100:0:1
+lowcov_spec=$4
+# 10000
+min_lowcov=$5
+# 100000:1000:100000000
+highcov_spec=$6
+# 100000
+min_highcov=$7
+# 10000:100:100000000
+highdeg_spec=$8
 
 echo "extracting low-coverage regions in $f"
 time odgi depth -i $f -s <(odgi paths -i $f -L | grep -v Cons) \
-     -w 100:0:1 -t 48 >$f.lowcov.bed
+     -w $lowcov_spec -t $threads >$f.lowcov.bed
 
 echo "extracting high-coverage regions in $f"
 time odgi depth -i $f -s <(odgi paths -i $f -L | grep -v Cons) \
-     -w 100000:1000:100000000 -t 48 >$f.highcov.bed
+     -w $highcov_spec -t $threads >$f.highcov.bed
 
 echo "burning low/high regions from $f"
 nolowhigh=$(basename $f .og).nolowhigh.og
-time odgi extract -i $f -t 48 -P \
+time odgi extract -i $f -t $threads -P \
      --inverse \
-     -b <(awk '$3 - $2 > 10000' $f.lowcov.bed ; \
-          awk '$3 - $2 > 100000' $f.highcov.bed ) \
-     -R <(odgi paths -i $f -L | grep '^chm13') \
+     -b <(awk '$3 - $2 > '$min_lowcov $f.lowcov.bed ; \
+          awk '$3 - $2 > '$min_highcov $f.highcov.bed ) \
+     -R <(odgi paths -i $f -L | grep '^'$ref) \
      -o - | odgi sort -O -i - -o $nolowhigh
 
 echo "extracting high-degree regions in $nolowhigh"
 time odgi degree -i $nolowhigh \
-     -w 10000:100:100000000 -t 48 >$f.highdeg.bed
+     -w $highdeg_spec -t $threads >$f.highdeg.bed
 
 echo "burning high-degree regions from $f"
 burned=$(basename $f .og).burn.og
-time odgi extract -i $nolowhigh -t 48 -P \
+time odgi extract -i $nolowhigh -t $threads -P \
      --inverse \
      -b $f.highdeg.bed \
      -R <(odgi paths -i $nolowhigh -L ) \
@@ -48,10 +60,11 @@ odgi view -i $burned -g >$burned_gfa
 
 echo "building the XG index for $burned_gfa"
 burned_xg=$(basename $burned .og).xg
-TEMPDIR=$(pwd) vg convert -t 48 -g -x $burned_gfa >$burned_xg
+TEMPDIR=$(pwd) vg convert -t $threads -g -x $burned_gfa >$burned_xg
 
 echo "building VCF from $burned_xg"
-burned_xg=$(basename $burned .og).vcf
-vg deconstruct -P chm13 \
-   $(for i in $(odgi paths -i $f -L | grep -v Cons | cut -f 1 -d '#' | sort | uniq ); do echo -n ' -A '$i; done) \
-   -e -a -t 48 $burned_xg >$burned_vcf
+burned_vcf=$(basename $burned .og).vcf
+vg deconstruct -P $ref \
+   $(for i in $(odgi paths -i $f -L | grep -v Cons | cut -f 1 -d '#' | sort | uniq ); \
+     do echo -n ' -A '$i; done) \
+   -e -a -t $threads $burned_xg >$burned_vcf
