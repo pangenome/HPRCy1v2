9e22132f57bb02d00b9a4a43667e09271946f6a3
Erik Garrison <erik.garrison@gmail.com>
2021-04-14 13:31:24 +0000

    update scripts

--- burn_extract.sh
+++ burn_extract.sh
@@ -1,5 +1,57 @@
 #!/bin/bash
 
+set -eo pipefail
+
 f=$1
-odgi extract -i $f -l <(odgi depth -i $f -w 1000 -d | awk 'NR > 1 && $2 <= 110 && $3 >= 3 && $3 <= 110 { print $1 }' ) \
-     -c 1 -R <(odgi paths -i $f -L | grep '^chm13\|^grch38' ) -o $(basename $f .og).burn.og -t 16
+
+echo "extracting low-coverage regions in $f"
+time odgi depth -i $f -s <(odgi paths -i $f -L | grep -v Cons) \
+     -w 100:0:1 -t 48 >$f.lowcov.bed
+
+echo "extracting high-coverage regions in $f"
+time odgi depth -i $f -s <(odgi paths -i $f -L | grep -v Cons) \
+     -w 100000:1000:100000000 -t 48 >$f.highcov.bed
+
+echo "burning low/high regions from $f"
+nolowhigh=$(basename $f .og).nolowhigh.og
+time odgi extract -i $f -t 48 -P \
+     --inverse \
+     -b <(awk '$3 - $2 > 10000' $f.lowcov.bed ; \
+          awk '$3 - $2 > 100000' $f.highcov.bed ) \
+     -R <(odgi paths -i $f -L | grep '^chm13') \
+     -o - | odgi sort -O -i - -o $nolowhigh
+
+echo "extracting high-degree regions in $nolowhigh"
+time odgi degree -i $nolowhigh \
+     -w 10000:100:100000000 -t 48 >$f.highdeg.bed
+
+echo "burning high-degree regions from $f"
+burned=$(basename $f .og).burn.og
+time odgi extract -i $nolowhigh -t 48 -P \
+     --inverse \
+     -b $f.highdeg.bed \
+     -R <(odgi paths -i $nolowhigh -L ) \
+     -o - | odgi sort -O -i - -o $burned
+
+echo "primary stats for $burned"
+odgi stats -i $burned -S | column -t
+
+echo "depth stats for $burned"
+odgi depth -i $burned -S | column -t
+
+echo "degree stats for $burned"
+odgi degree -i $burned -S | column -t
+
+echo "generating GFA for $burned"
+burned_gfa=$(basename $burned .og).gfa
+odgi view -i $burned -g >$burned_gfa
+
+echo "building the XG index for $burned_gfa"
+burned_xg=$(basename $burned .og).xg
+TEMPDIR=$(pwd) vg convert -t 48 -g -x $burned_gfa >$burned_xg
+
+echo "building VCF from $burned_xg"
+burned_xg=$(basename $burned .og).vcf
+vg deconstruct -P chm13 \
+   $(for i in $(odgi paths -i $f -L | grep -v Cons | cut -f 1 -d '#' | sort | uniq ); do echo -n ' -A '$i; done) \
+   -e -a -t 48 $burned_xg >$burned_vcf
--- plot_coverage_density.sh
+++ plot_coverage_density.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 f=$1
-Rscript -e 'require(tidyverse); c <- read.delim("'$f'"); ggplot(c, aes(x=coverage)) + geom_density() + scale_x_log10() + labs(title="'$f'"); ggsave("'$f'.density.png", height=5, width=8)'
+Rscript -e 'require(tidyverse); c <- read.delim("'$f'"); ggplot(c, aes(x=coverage)) + geom_density() + scale_x_log10() + labs(title="'$f'"); ggsave("'$f'.density.xlog10.png", height=5, width=8)'
+Rscript -e 'require(tidyverse); c <- read.delim("'$f'"); ggplot(c, aes(x=coverage)) + geom_density() + xlim(0,200) + labs(title="'$f'"); ggsave("'$f'.density.lt200.png", height=5, width=8)'
+Rscript -e 'require(tidyverse); c <- read.delim("'$f'"); ggplot(c, aes(x=coverage)) + geom_histogram(binwidth=1) + xlim(0,200) + labs(title="'$f'"); ggsave("'$f'.hist.lt200.png", height=5, width=8)'
